3ca16777cabd09ef8f8997fc8a4f0c6e76ae167f
Steelswheel <50166539+Steelswheel@users.noreply.github.com>
2020-05-28 00:34:44 +0500

    ver 2.0

--- scripts/subscribe.js
+++ scripts/subscribe.js
@@ -1,28 +1,14 @@
-const originInfoFrom = document.body.querySelector('.origin-info-text-from');//Список моих подписок со страницы
-const originInfoTo = document.body.querySelector('.origin-info-text-to');//Список подписчиков со страницы
-const totalInfo = document.body.querySelector('.total-info-text');//Итоговый результат
-const submitButton = document.body.querySelector('.submit');//Кнопка отправки данных
-const clearButton = document.body.querySelector('.clear');//Кнопка очистки формы
+const originInfoFrom = document.body.querySelector('.subscribes_my-list');//Список моих подписок со страницы
+const originInfoTo = document.body.querySelector('.subscribes_subscribers-list');//Список подписчиков со страницы
+const totalInfo = document.body.querySelector('.subscribes_total');//Итоговый результат
+const submitButton = document.body.querySelector('.subscribes_submit');//Кнопка отправки данных
+const clearButton = document.body.querySelector('.subscribes_clear');//Кнопка очистки формы
 
 function dataFilter(str) {//Функция для получения имени аккаунта
   const arr = str.split('Фото профиля ').filter(item => item != '').map(item => item.split('\n', 1).join(''));
   const set = new Set(arr);
   return Array.from(set);
 };
-
-function sortSubs(arr1, arr2) {
-  const sorted = [];
-  for (a in arr1) {
-    for (let i = 0; i < arr2.length; i++) {
-      if (a == arr2[i]) {
-        continue;
-      } else {
-        sorted.push(a);
-      }
-    }
-  }
-  return sorted;
-}
 
 function sortSubs(a, b) {
   const arr = a.filter(i => b.indexOf(i) == -1);
@@ -42,7 +28,7 @@ submitButton.addEventListener('click', () => {
   for (let i = 0; i < sorted.length; i++) {
     let br = document.createElement('br');
     let a = document.createElement('a');
-    a.setAttribute('href', `http://instagram.com/${sorted[i]}`);
+    a.setAttribute('href', `https://instagram.com/${sorted[i]}`);
     a.innerHTML = `${sorted[i]}`;
     totalInfo.appendChild(a);
     totalInfo.appendChild(br);
